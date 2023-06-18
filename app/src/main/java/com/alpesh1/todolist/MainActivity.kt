99638d40a8acfb1bd9fd048e54178543824edb64
Alpesh Rathava <alpeshrathava301@gmail.com>
2023-06-18 16:16:24 +0530

    commit design2

--- app/src/main/java/com/alpesh1/todolist/MainActivity.kt
+++ app/src/main/java/com/alpesh1/todolist/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.alpesh1.todolist
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var category = arrayOf("Time Management","Calling","Work","Personal","Shopping","Wishlist")
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,category)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                Toast.makeText(applicationContext, "selected category"+category[p2], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+
     }
 }
