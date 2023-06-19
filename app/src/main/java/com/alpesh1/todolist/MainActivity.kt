0dae2503d45749e847aede059ed8b7ef35f8d164
Darshan26B <iamdarshan6239db@gmail.com>
2023-06-19 18:22:21 +0530

    commit3

--- app/src/main/java/com/alpesh1/todolist/MainActivity.kt
+++ app/src/main/java/com/alpesh1/todolist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.alpesh1.todolist
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,11 +15,19 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    var category = arrayOf("Time Management","Calling","Work","Personal","Shopping","Wishlist")
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.addbtn.setOnClickListener {
+          var  intent=Intent(this,new_task_add::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
