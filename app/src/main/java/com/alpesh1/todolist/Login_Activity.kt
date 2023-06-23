a8707affe5c61b10f14d88034536dac1bc253816
pratik465 <pratikgangadiya465@gmail.com>
2023-06-23 15:01:31 +0530

    commit 7

--- app/src/main/java/com/alpesh1/todolist/Login_Activity.kt
+++ app/src/main/java/com/alpesh1/todolist/Login_Activity.kt
@@ -1,11 +1,22 @@
 package com.alpesh1.todolist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Login_Activity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-    }
+
+
+     }
 }
