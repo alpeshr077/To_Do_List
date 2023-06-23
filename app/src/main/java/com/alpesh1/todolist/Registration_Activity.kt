0cc64613c7738edf980e82435398cdd2fcaa4e76
pratik465 <pratikgangadiya465@gmail.com>
2023-06-23 01:21:45 -0700

    commit8

---
+++ app/src/main/java/com/alpesh1/todolist/Registration_Activity.kt
@@ -0,0 +1,11 @@
+package com.alpesh1.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registration_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
